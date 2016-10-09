858564e45b6ac68d1fb7aaa53bf4894de21466ca
shiyaorong <vivian@raintr.com>
2016-10-09 14:12:23 +0800

    二维码

--- admin/js/global.js
+++ admin/js/global.js
@@ -1,7 +1,6 @@
-
 function getCheckLogin() {
 	var sd = localStorage.getItem("admin_login");
-	if (sd != undefined && sd.length != 0) {
+	if(sd != undefined && sd.length != 0) {
 		return true;
 	} else {
 		return false;
@@ -9,6 +8,26 @@ function getCheckLogin() {
 }
 
 function getHeadUrl() {
-//	return "http://192.168.2.104/v1/";
+	//	return "http://192.168.2.104/v1/";
 	return "http://interface.pinshe.org/v1/";
+}
+
+function utf16to8(str) { //转码
+	var out, i, len, c;
+	out = "";
+	len = str.length;
+	for(i = 0; i < len; i++) {
+		c = str.charCodeAt(i);
+		if((c >= 0x0001) && (c <= 0x007F)) {
+			out += str.charAt(i);
+		} else if(c > 0x07FF) {
+			out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+			out += String.fromCharCode(0x80 | ((c >> 6) & 0x3F));
+			out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+		} else {
+			out += String.fromCharCode(0xC0 | ((c >> 6) & 0x1F));
+			out += String.fromCharCode(0x80 | ((c >> 0) & 0x3F));
+		}
+	}
+	return out;
 }
--- admin/store.js
+++ admin/store.js
@@ -1,22 +1,22 @@
 var app = angular.module('admin', []);
 app.controller('store', function($scope, $http) {
-	
-	if (!getCheckLogin()) {
-		location.href = "admin_login.html";
-	}
-	
+
+	//	if (!getCheckLogin()) {
+	//		location.href = "admin_login.html";
+	//	}
+
 	$http.get(getHeadUrl() + "commodity.a?page=1").success(function(response) {
 		$scope.commodityList = response.body.array;
-		
+
 		$http.get(getHeadUrl() + "store.a?page=" + (GetQueryInt("page") == 0 ? 1 : GetQueryInt("page"))).success(
 			function(response) {
 				$scope.json = response.body.array;
 			}
 		);
-	
-	});	
-	
-	$scope.modify = function(index){
+
+	});
+
+	$scope.modify = function(index) {
 		var id = $("guid" + index).value;
 		var mid = $("mid" + index).value;
 		var name = $("name" + index).value;
@@ -27,7 +27,7 @@ app.controller('store', function($scope, $http) {
 		var address = $("address" + index).value;
 		var longitude = $("longitude" + index).value;
 		var latitude = $("latitude" + index).value;
-//		var rank = $("rank" + index).value;
+		//		var rank = $("rank" + index).value;
 		var dateStr = $("date" + index).value;
 		var owner = $("owner" + index).value;
 		var slogan = $("slogan" + index).value;
@@ -40,22 +40,24 @@ app.controller('store', function($scope, $http) {
 		var commentStr = $("comment" + index).value;
 		var description = $("description" + index).value;
 		$http({
-			method:'POST',
+			method: 'POST',
 			url: getHeadUrl() + "store_modify.a",
 			data: "id=" + id + "&mid=" + mid + "&name=" + name + "&payment=" + payment + "&invaild=" + invaild + "&phone=" + phone + "&address=" + address + "&longitude=" + longitude + "&latitude=" + latitude + "&date=" + dateStr + "&owner=" + owner + "&slogan=" + slogan + "&recommend=" + recommend + "&feature1=" + feature1 + "&feature2=" + feature2 + "&feature3=" + feature3 + "&star=" + star + "&activity=" + activity + "&comment=" + commentStr + "&description=" + description,
-			headers:{'Content-Type': 'application/x-www-form-urlencoded'}
-		}).error(function(data,state){
-            alert("信息过长");
-		}); 
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		}).error(function(data, state) {
+			alert("信息过长");
+		});
 	}
-	
+
 	$scope.qrcode = function(model) {
 		$http.get(getHeadUrl() + "wechat_qrcode.a?sid=" + model.guid).success(
 			function(response) {
 				model.qrcodeUrl = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=" + response.ticket;
 			}
 		);
-		
+
 	}
-	
+
 });
